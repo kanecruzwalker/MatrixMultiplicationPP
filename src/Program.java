4402eee4167a4a9cf79da4f0ca09c1c14adacac0
Kane Cruz-Walker <kanercruzwalker@hotmail.com>
2022-03-19 03:13:36 -0400

    Initial project spects and functionality. Update Needed: Finding and displaying connections, user input matrices, good ui.

--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,47 @@
 public class Program {
     public static void main(String[] args) {
+        //Dimension of matrices in use
+        int rowAndCol = 4;
 
+        //Program Matrices into code
+
+        //2 Matrices to be used
+        int[][] matrixA = {{1,0,0,1},{0,1,0,1},{0,0,1,0},{1,1,1,0}};
+        int[][] matrixB = {{0,1,1,1},{1,1,0,1},{0,1,0,1},{1,0,1,0}};
+
+        //Get Matrix of 2 matrices multiplied (Product of two patrices)
+        int[][] newMatrix = getMultipliedMatrices(matrixA, matrixB, rowAndCol);
+
+        //Output new matrix created when multiplying 2 matrices
+        displayProduct(newMatrix);
+
+
+        //------------To Do
+        //Output number of connections within the directed graph being depicted
+        //Get User Input as well as hard coded
+    }
+
+    //Pass 2 Matrices return a Matrix of the product
+    public static int[][] getMultipliedMatrices(int[][] matrixA, int[][] matrixB, int rowAndColLength){
+        int[][] resultingMatrix = new int[rowAndColLength][rowAndColLength];
+        for(int i = 0; i < rowAndColLength; i++){
+            for(int j = 0; j < rowAndColLength; j++){
+                for(int k = 0; k < rowAndColLength; k++){
+                    resultingMatrix[i][j] += matrixA[i][k] * matrixB[k][j];
+                }
+            }
+        }
+        return resultingMatrix;
+    }
+
+    //Display a Matrix
+    public static void displayProduct(int[][] product){
+        System.out.println("Product of two matrices is: ");
+        for(int[] row : product){
+            for(int column : row){
+                System.out.print(column + "   ");
+            }
+            System.out.println();
+        }
     }
 }
